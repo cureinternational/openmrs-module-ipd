7a729cb9ea6bcc43589988ba19bcd0510b48a0c8
Soorya Kumaran C <90232857+SooryaKumaranC-tw@users.noreply.github.com>
2025-11-20 12:51:12 +0530

    Add Amend and Acknowledge note for a  medication administration note (#3)

--- api/src/main/java/org/openmrs/module/ipd/api/service/CareTeamService.java
+++ api/src/main/java/org/openmrs/module/ipd/api/service/CareTeamService.java
@@ -5,8 +5,6 @@ import org.openmrs.api.APIException;
 import org.openmrs.api.OpenmrsService;
 import org.openmrs.module.ipd.api.model.CareTeam;
 
-
-
 public interface CareTeamService extends OpenmrsService {
 
     CareTeam saveCareTeam(CareTeam careTeam) throws APIException;
