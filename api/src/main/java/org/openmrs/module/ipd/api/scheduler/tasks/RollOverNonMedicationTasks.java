5a0eb47c4579ac60c0ee553a4b021179b1e385fe
Pooja Sastry <pooja.sastry@thoughtworks.com>
2025-12-30 10:34:19 +0530

    Pooja | Fix error for tests

--- api/src/main/java/org/openmrs/module/ipd/api/scheduler/tasks/RollOverNonMedicationTasks.java
+++ api/src/main/java/org/openmrs/module/ipd/api/scheduler/tasks/RollOverNonMedicationTasks.java
@@ -5,6 +5,9 @@ import org.openmrs.module.ipd.api.events.IPDEventManager;
 import org.openmrs.module.ipd.api.events.model.IPDEvent;
 import org.openmrs.module.ipd.api.events.model.IPDEventType;
 import org.openmrs.scheduler.tasks.AbstractTask;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 @Component
 public class RollOverNonMedicationTasks extends AbstractTask implements ApplicationContextAware {
@@ -17,6 +20,11 @@ public class RollOverNonMedicationTasks extends AbstractTask implements Applicat
             IPDEvent ipdEvent = new IPDEvent(null, null, eventType);
             eventManager.processEvent(ipdEvent);
         }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        // Implement if needed for Spring context injection
     }
 
 }
