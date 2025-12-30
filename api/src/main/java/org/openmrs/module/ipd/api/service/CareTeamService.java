97791946817848505386cbb4ae655c3ffa878526
Pooja Sastry <pooja.sastry@thoughtworks.com>
2025-12-30 10:43:46 +0530

    A1208102423475700 | Enable Add to Drug Chart if order is dispensed (#76)

--- api/src/main/java/org/openmrs/module/ipd/api/service/CareTeamService.java
+++ api/src/main/java/org/openmrs/module/ipd/api/service/CareTeamService.java
@@ -4,11 +4,6 @@ import org.openmrs.Visit;
 import org.openmrs.api.APIException;
 import org.openmrs.api.OpenmrsService;
 import org.openmrs.module.ipd.api.model.CareTeam;
-import org.openmrs.module.ipd.api.model.Schedule;
-import org.springframework.stereotype.Service;
-import org.openmrs.api.OpenmrsService;
-
-
 
 public interface CareTeamService extends OpenmrsService {
 
